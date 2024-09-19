8d9207c3447598b4690847ec06c0f10b617e0783
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-09-19 09:04:22 +0300

    add: Add filter

--- client/src/entities/markAnnotation/api/useMarkAnnotationQuery.ts
+++ client/src/entities/markAnnotation/api/useMarkAnnotationQuery.ts
@@ -3,14 +3,16 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 
 import { IMarkAnnotation } from "../model/types/markAnnotationTypes";
 
-export const useMarkAnnotationQuery = () => {
+export const useMarkAnnotationQuery = (mark_id?: string) => {
   const client = useQueryClient();
+
+  const filter = mark_id ? `?mark_id=${mark_id}` : "";
 
   const { data, isLoading } = useQuery({
     queryKey: ["mark annotations"],
     queryFn: () =>
       axios.get<IMarkAnnotation[]>(
-        `${process.env.REACT_APP_URL_API}/api/v1/mark-annotations/`,
+        `${process.env.REACT_APP_URL_API}/api/v1/mark-annotations/${filter}`,
       ),
   });
 
